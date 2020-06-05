7e191fc7b983f436168bdec04ce40daa74be7752
tuliohs <tuliohhenriquepaz@gmail.com>
2020-06-04 22:54:30 -0300

    innit

---
+++ SocketSharp/Program.cs
@@ -0,0 +1,20 @@
+﻿
+using System;
+using WebSocketSharp;
+
+namespace SocketSharp
+{
+  public class SocketSharp
+  {
+    public static void Main (string[] args)
+    {
+            Console.WriteLine("Hello World!");
+      using (var ws = new WebSocket ("http://localhost:3000")) {
+       /* ws.OnMessage += (sender, e) =>
+          Console.WriteLine ("Laputa says: " + e.Data);*/
+
+        ws.Connect ();
+      }
+    }
+  }
+}
